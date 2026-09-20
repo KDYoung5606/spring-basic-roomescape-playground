d9096353e227c72fca6b26ff0d5418b8afde8320
KDYoung5606 <2271323@hansung.ac.kr>
2026-09-20 23:59:07 +0900

    feat: JPA 설정 적용 및 엔티티 매핑

--- src/main/java/roomescape/theme/Theme.java
+++ src/main/java/roomescape/theme/Theme.java
@@ -1,9 +1,20 @@
 package roomescape.theme;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Theme {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private boolean deleted;
 
     public Theme() {
     }
--- src/main/java/roomescape/time/Time.java
+++ src/main/java/roomescape/time/Time.java
@@ -1,8 +1,20 @@
 package roomescape.time;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Time {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "time_value")
     private String value;
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private boolean deleted;
 
     public Time(Long id, String value) {
         this.id = id;

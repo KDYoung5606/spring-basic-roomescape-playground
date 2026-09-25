59f3910ec4415f22fd3905adde2eed1026e9bcdd
KDYoung5606 <2271323@hansung.ac.kr>
2026-09-26 04:04:21 +0900

    refactor: ReservationRepository.findAll에 EntityGraph 적용

--- src/main/java/roomescape/reservation/ReservationRepository.java
+++ src/main/java/roomescape/reservation/ReservationRepository.java
@@ -1,10 +1,15 @@
 package roomescape.reservation;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    @Override
+    @EntityGraph(attributePaths = {"member", "time", "theme"})
+    List<Reservation> findAll();
 
     List<Reservation> findByDateAndThemeId(String date, Long themeId);
 
